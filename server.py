5a31fb5738e2bb902ba705dd7e6c1ad196d21fb0
kombinat <kombinat@debian-lifebook>
2017-05-27 12:21:41 +0200

    Menü kann jetzt geöfnet werden.

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ x=0
 app=flask.Flask("Vokabeltrainer")
 
 
-Vokabel= list(csv.reader(open("Vokabeln.csv"),delimiter=";"))
+Vokabel= list(csv.reader(open("Franzoesisch.csv")))
 #Vokabel=[["ich","moi"],["du","tu"],["Computer","ordinateur"]]
 
 richtig=[]
@@ -29,7 +29,7 @@ def lernen():
     if antwort:
         x=x+1
         antwort_id=flask.request.args.get("antwort_id")
-        if antwort==Vokabel[int(antwort_id)][1]:
+        if antwort==Vokabel[int(antwort_id)][0]:
             result="richtig"
             richtig.append(int(antwort_id))
             msg="Ja,das ist richtig!"
@@ -42,8 +42,31 @@ def lernen():
     r=random.randint(0,len(Vokabel)-1)
     while r in richtig:
         r=random.randint(0,len(Vokabel)-1)
-    auswahl=Vokabel[r][0]
+    auswahl=Vokabel[r][1]
     return flask.render_template("lernen.html",frage=auswahl,id=r,msg=msg,result=result,status=status)
 
-
+@app.route ("/menue/")
+def menue():
+	return flask.render_template("menue.html")
+	
+@app.route ("/ersteFremdsprache/")
+def ersteFremdsprache():
+	return
+	
+@app.route ("/zweiteFremdsprache/")
+def zweiteFremdsprache():
+	return flask.render_template("franzoesisch.html")
+@app.route ("/zweiteFremdsprache/neu/")
+def addzwei():
+	wort_fran=flask.request.args.get("fran")
+	wort_deut=flask.request.args.get("deut")
+	spalten=["fran","deut"]
+	vokabelliste=csv.DictWriter(open("Franzoesisch.csv","a"), spalten)
+	vokabelliste.writerow({"fran":wort_fran,"deut":wort_deut})
+	vokabelliste=csv.DictReader(open("Franzoesisch.csv"),spalten)
+	vokabelliste_neu=[]
+	for vokabelpaar in vokabelliste:
+		vokabelliste_neu.append([vokabelpaar["fran"],vokabelpaar["deut"]])
+		
+	return flask.render_template("franzoesisch.html",vokabelliste=vokabelliste_neu)
 
